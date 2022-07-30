494b57fc2eabe3f7c3832b8fa032c49dd458cba6
Moonlit Knight <roy.smith.esq+gh@gmail.com>
2022-07-30 02:49:23 +0200

    Create ec2boot.sh

---
+++ ec2boot.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+#
+# ec2 boot script
+# see https://linuxconfig.org/how-to-run-script-on-startup-on-ubuntu-20-04-focal-fossa-server-desktop
+# installed with ...
+#
+#
+set -x
+hostname=`hostname`
+if [ "${hostname:-:3}" == "ec2" ]
+then
+        echo "hostname $hostname"
+else
+        hostname ec2d
+fi
+systemctl stop docker           # we don't want docker running until shared is mounted
+if [ ! -f /shared/jails ]       # if shared not mounted, wait to give me time to attach the volume, then rerun
+then
+        sleep 2
+        mount /shared
+        /ec2boot.sh
+        exit
+fi
+# do the startup stuff
+echo starting docker
+
+echo running the rest of the startup stuff
