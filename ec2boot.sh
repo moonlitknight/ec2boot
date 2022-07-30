c4bf8fd726a93441f901691f0865cfda155c6b44
Moonlit Knight <roy.smith.esq+gh@gmail.com>
2022-07-30 13:13:35 +0200

    Update ec2boot.sh

--- ec2boot.sh
+++ ec2boot.sh
@@ -36,10 +36,8 @@ fi
 # do the startup stuff
 echo running the rest of the startup stuff in 1 minute
 at now + 2 minutes << EOF
- PATH=$PATH:/bin:/usr/bin:/sbin:/usr/sbin
- systemctl restart docker
- /shared/scripts/d_nagios
- /shared/scripts/d_nginx
+ PATH=$PATH:/bin:/usr/bin:/sbin:/usr/sbin:/shared/scripts
+ /shared/scripts/jailboot.sh all
 EOF
 atq
 
